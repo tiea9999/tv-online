1eeac80bdfeb7a2ed861a5804f8941a59e94a3eb
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-03-13 23:15:52 +0700

    Create channels.js

---
+++ channels.js
@@ -0,0 +1,27 @@
+const channels={
+
+tv:[
+{
+name:"MONO MAX1",
+logo:"https://www.fileinwtv.site/postor/20250830165328Mono1.jpg",
+url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+}
+],
+
+movie:[
+{
+name:"Movie Channel",
+logo:"https://cdn-icons-png.flaticon.com/512/3659/3659899.png",
+url:"https://test-streams.mux.dev/test_001/stream.m3u8"
+}
+],
+
+series:[
+{
+name:"Series Channel",
+logo:"https://cdn-icons-png.flaticon.com/512/3659/3659784.png",
+url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+}
+]
+
+};
