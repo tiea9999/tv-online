7d6b6a3b2bc6285d3544fc0f40b6dec9cdfb04ed
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-03-14 01:07:19 +0700

    Create m3u-loader.js

---
+++ m3u-loader.js
@@ -0,0 +1,52 @@
+async function loadM3U(file){
+
+const response = await fetch(file);
+const text = await response.text();
+
+const lines = text.split("\n");
+
+let name="";
+let logo="";
+
+const container=document.getElementById("channels");
+
+for(let i=0;i<lines.length;i++){
+
+const line=lines[i].trim();
+
+if(line.startsWith("#EXTINF")){
+
+const nameMatch=line.match(/,(.*)/);
+if(nameMatch) name=nameMatch[1];
+
+const logoMatch=line.match(/tvg-logo="(.*?)"/);
+if(logoMatch) logo=logoMatch[1];
+
+}
+
+if(line.startsWith("http")){
+
+const url=line;
+
+const card=document.createElement("div");
+card.className="channel-card";
+
+card.innerHTML=`
+
+<div onclick="openPlayer('${url}')">
+
+<img src="${logo}" onerror="this.src='https://cdn-icons-png.flaticon.com/512/149/149071.png'">
+
+<div>${name}</div>
+
+</div>
+
+`;
+
+container.appendChild(card);
+
+}
+
+}
+
+}
