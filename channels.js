99bb97aaacbdc0f26a86c1472aa43bf6c5f59a31
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-03-14 12:59:08 +0700

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,50 +1,31 @@
 const channels = {
 
-tv:{
+tv: {
 
-digital:[
-
+digital: [
 {
 name:"MONO29",
 logo:"https://www.fileinwtv.site/postor/20250830165328Mono1.jpg",
 url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
 },
-
 {
 name:"CH3 HD",
 logo:"https://upload.wikimedia.org/wikipedia/th/7/7a/Channel_3_HD.png",
 url:"https://test-streams.mux.dev/test_001/stream.m3u8"
 }
-
 ],
 
-movie:[],
-sport:[],
-documentary:[],
-cartoon:[]
+movie: [],
+sport: [],
+documentary: [],
+cartoon: []
 
 },
 
-movie:[
-
-{
-name:"Movie Channel",
-logo:"https://cdn-icons-png.flaticon.com/512/3659/3659899.png",
-url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-}
-
-],
-
-series:[
-
-{
-name:"Series Channel",
-logo:"https://cdn-icons-png.flaticon.com/512/3659/3659784.png",
-url:"https://test-streams.mux.dev/test_001/stream.m3u8"
-}
-
-]
+movie: [],
+series: []
 
 };
+
 
 
