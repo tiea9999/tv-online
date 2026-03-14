a047b73ce2a19fa0f4568de1219c5e9317a76dbe
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-03-14 10:18:26 +0700

    Update channels.js

--- channels.js
+++ channels.js
@@ -5,63 +5,63 @@ tv: {
 digital: [
 
 {
-name: "MONO29",
-logo: "https://www.fileinwtv.site/postor/mono29.jpg",
-url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+name:"CH3 HD",
+logo:"https://upload.wikimedia.org/wikipedia/th/7/7a/Channel_3_HD.png",
+url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
 },
 
 {
-name: "CH3 HD",
-logo: "https://www.fileinwtv.site/postor/ch3.jpg",
-url: "https://test-streams.mux.dev/test_001/stream.m3u8"
+name:"MONO29",
+logo:"https://upload.wikimedia.org/wikipedia/th/2/2e/Mono29_Logo.png",
+url:"https://test-streams.mux.dev/test_001/stream.m3u8"
 }
 
 ],
 
-movie: [
+movie:[
 
 {
-name: "MONO MAX",
-logo: "https://www.fileinwtv.site/postor/20250830165328Mono1.jpg",
-url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+name:"MONO MAX",
+logo:"https://www.fileinwtv.site/postor/20250830165328Mono1.jpg",
+url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
 }
 
 ],
 
-sport: [
+sport:[
 
 {
-name: "True Sport 1",
-logo: "https://www.fileinwtv.site/postor/20240418192933TrueSport1.jpg",
-url: "https://test-streams.mux.dev/test_001/stream.m3u8"
+name:"True Sport 1",
+logo:"https://www.fileinwtv.site/postor/20240418192933TrueSport1.jpg",
+url:"https://test-streams.mux.dev/test_001/stream.m3u8"
 }
 
 ],
 
-documentary: [
+documentary:[
 
 {
-name: "Discovery",
-logo: "https://cdn-icons-png.flaticon.com/512/2920/2920349.png",
-url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+name:"Discovery",
+logo:"https://cdn-icons-png.flaticon.com/512/2920/2920349.png",
+url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
 }
 
 ],
 
-cartoon: [
+cartoon:[
 
 {
-name: "Cartoon Network",
-logo: "https://cdn-icons-png.flaticon.com/512/3659/3659784.png",
-url: "https://test-streams.mux.dev/test_001/stream.m3u8"
+name:"Cartoon Network",
+logo:"https://cdn-icons-png.flaticon.com/512/3659/3659784.png",
+url:"https://test-streams.mux.dev/test_001/stream.m3u8"
 }
 
 ]
 
 },
 
-movie: [],
-series: []
+movie:[],
+series:[]
 
 };
 
