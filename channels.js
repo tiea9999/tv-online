a7f042a5d0c7bd0c10a3ff03dbc54392726ae600
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-03-14 16:39:32 +0700

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,13 +1,36 @@
 const channels={
 
 tv:{
+
 digital:[
 {
 name:"CH7 HD",
 logo:"https://upload.wikimedia.org/wikipedia/th/1/1b/Channel_7_HD.png",
 url:"https://example.com/ch7.m3u8"
+},
+{
+name:"MONO29",
+logo:"https://upload.wikimedia.org/wikipedia/th/4/4f/Mono29.png",
+url:"https://example.com/mono.m3u8"
+}
+],
+
+movie:[
+{
+name:"HBO",
+logo:"https://upload.wikimedia.org/wikipedia/commons/6/6a/HBO_logo.svg",
+url:"https://example.com/hbo.m3u8"
+}
+],
+
+sport:[
+{
+name:"PPTV HD",
+logo:"https://upload.wikimedia.org/wikipedia/th/2/2c/PPTV_HD36.png",
+url:"https://example.com/pptv.m3u8"
 }
 ]
+
 },
 
 movie:[
