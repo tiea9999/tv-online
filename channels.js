947f911d1e2d3de4feae14c312bf4acf38f6f169
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-03-14 18:43:10 +0700

    Update channels.js

--- channels.js
+++ channels.js
@@ -3,47 +3,107 @@ const channels={
 tv:{
 
 digital:[
-{
-name:"CH7 HD",
-logo:"https://upload.wikimedia.org/wikipedia/th/1/1b/Channel_7_HD.png",
-url:"https://example.com/ch7.m3u8"
-},
-{
-name:"MONO29",
-logo:"https://upload.wikimedia.org/wikipedia/th/4/4f/Mono29.png",
-url:"https://example.com/mono.m3u8"
+      { name:"3HD", logo:"https://www.fileinwtv.site/postor/20240418191821ch3hd.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-ch3_720/chunks.m3u8"},
+      { name:"5HD", logo:"https://www.fileinwtv.site/postor/20240418185850ch5hd.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-ch5_720/chunks.m3u8"},
+      { name:"7HD", logo:"https://www.fileinwtv.site/postor/20240418185909ch7hd.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-ch7_720/chunks.m3u8"},
+      { name:"TNN16", logo:"https://www.fileinwtv.site/postor/20240418190014TNN16.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/sd-tnn24_720/chunks.m3u8"},
+      { name:"Nation TV", logo:"https://www.fileinwtv.site/postor/20240418192308Nation1.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/sd-nation_720/chunks.m3u8" },
+      { name:"True4U", logo:"https://www.fileinwtv.site/postor/20240418190115True4u.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/sd-24u_480/chunks.m3u8" },
+      { name:"GMM25", logo:"https://www.fileinwtv.site/postor/20240418190128gmmchannel.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/sd-gmmchannel_720/chunks.m3u8" },
+      { name:"Workpoint TV", logo:"https://www.fileinwtv.site/postor/20240418190941Workpoint.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-workpoint_720/chunks.m3u8" },
+      { name:"MONO29", logo:"https://www.fileinwtv.site/postor/20240418191023mono29.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-mono_720/chunks.m3u8" },
+      { name:"MCOT HD", logo:"https://www.fileinwtv.site/postor/20240418191042mcothd.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-mcot_720/chunks.m3u8" },
+      { name:"Thairath TV", logo:"https://www.fileinwtv.site/postor/20241201222710thairahttvhd2.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-thairath_720/chunks.m3u8" },
+      { name:"AmarinTV", logo:"https://www.fileinwtv.site/postor/20240418191809amarintvhd.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-amarin_720/chunks.m3u8" },
+      { name:"Thai PBS", logo:"https://www.fileinwtv.site/postor/20240418191223tpbshd.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-tpbs_720/chunks.m3u8" },
+      { name:"PPTV", logo:"https://www.fileinwtv.site/postor/20240418191253pptvhd.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-pptv_720/chunks.m3u8" },
+      { name:"ONE31", logo:"https://www.fileinwtv.site/postor/20240418191350onehd.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-gmmone_720/chunks.m3u8"},
+      { name:"CH8", logo:"https://www.fileinwtv.site/postor/20240418191335ch8.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-ch8_480/chunks.m3u8"},
+      { name:"NBT 2 HD", logo:"https://www.fileinwtv.site/postor/20240418191323nbthd.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/sd-nbt_720/chunks.m3u8"
+
 }
 ],
 
 movie:[
 {
-name:"HBO",
-logo:"https://upload.wikimedia.org/wikipedia/commons/6/6a/HBO_logo.svg",
-url:"https://example.com/hbo.m3u8"
+      { name:"HBO", url:"https://love.pifipa2974.workers.dev/lx-origin/th-hbo_720/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418201334HBO.jpg" },
+      { name:"HBO Hits", url:"https://love.pifipa2974.workers.dev/lx-origin/th-hbohits_720/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418201343HBO_Hits.jpg" },
+      { name:"HBO Signature", url:"https://love.pifipa2974.workers.dev/lx-origin/th-hbosignature_720/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418201358HBO_Signature.jpg" },
+      { name:"HBO Family", url:"https://love.pifipa2974.workers.dev/lx-origin/th-hbofamily_720/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418201412HBO_Family.jpg" },
+      { name:"Cinemax", url:"https://love.pifipa2974.workers.dev/lx-origin/th-cinemax_720/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418201426cinemax.jpg" },
+      { name:"WarnerTV", url:"https://love.pifipa2974.workers.dev/lx-origin/hd-warnertv_720/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418201607WBTV.jpg" },
+      { name:"AXN", url:"https://love.pifipa2974.workers.dev/vx-origin/th-axn_720/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418201633AXN.jpg" },
+       { name:"true moviehits", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-moviehits_720/chunks.m3u8", logo:"https://dookeela2.live/uploads/1719803882.png" },
+
+{ name:"true thaifilm", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-thaifilm_720/chunks.m3u8", logo:"https://dookeela2.live/uploads/1706539896.png" },
+
+{ name:"true film asia", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-filmasia_720/chunks.m3u8", logo:"https://dookeela2.live/uploads/1719205705.png" },
+
+{ name:"ccm", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-celestial_480/chunks.m3u8", logo:"https://dookeela2.live/uploads/1719803240.png" },
+
+{ name:"true series", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-series_720/chunks.m3u8", logo:"https://dookeela2.live/uploads/1719205991.png" },
+
+{ name:"true asia more", url:"https://love.pifipa2974.workers.dev/lx-origin/hd-asianmore_720/chunks.m3u8", logo:"https://dookeela2.live/uploads/1719205850.png" },
+
+{ name:"true x-zyte", url:"https://love.pifipa2974.workers.dev/lx-origin/hd-xzyte_720/chunks.m3u8", logo:"https://dookeela2.live/uploads/1719206155.png" 
+
 }
 ],
 
 sport:[
-{
-name:"PPTV HD",
-logo:"https://upload.wikimedia.org/wikipedia/th/2/2c/PPTV_HD36.png",
-url:"https://example.com/pptv.m3u8"
+      { name:"MONO MAX1", logo:"https://www.fileinwtv.site/postor/20250830165328Mono1.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-monomax01_480/chunks.m3u8" },
+      { name:"MONO MAX2", logo:"https://www.fileinwtv.site/postor/20250830165343Mono2.jpg", url:"https://love.pifipa2974.workers.dev/lx-origin/th-monomax02_480/chunks.m3u8" },
+      { name:"MONO MAX3", logo:"https://www.fileinwtv.site/postor/20250830165408Mono3.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-monomax03_480/chunks.m3u8" },
+      { name:"MONO MAX4", logo:"https://www.fileinwtv.site/postor/20250830165433Mono4.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-monomax04bkp_720/chunks.m3u8" },
+      { name:"MONO MAX5", logo:"https://www.fileinwtv.site/postor/20250830165451Mono5.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-monomax05_480/chunks.m3u8" },
+      { name:"MONO MAX6", logo:"https://www.fileinwtv.site/postor/20250830165513Mono6.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-monomax06_480/chunks.m3u8" },
+      { name:"MONO MAX7", logo:"https://www.fileinwtv.site/postor/20250830165536Mono7.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-monomax07_480/chunks.m3u8" },
+      { name:"MONO MAX8", logo:"https://www.fileinwtv.site/postor/20250830165621Mono8.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-monomax08_480/chunks.m3u8" },
+
+      { name:"Bein Sport1", logo:"https://www.fileinwtv.site/postor/20240418192615Bein1.jpg", url:"https://love.r95lvvlb9o.workers.dev/vx-origin/epl-bein1_480/chunks.m3u8" },
+      { name:"Bein Sport2", logo:"https://www.fileinwtv.site/postor/20240418192624Bein2.jpg", url:"https://love.r95lvvlb9o.workers.dev/vx-origin/epl-bein2_480/chunks.m3u8" },
+      { name:"Bein Sport3", logo:"https://www.fileinwtv.site/postor/20240418192653Bein3.jpg", url:"https://love.r95lvvlb9o.workers.dev/vx-origin/epl-bein3_480/chunks.m3u8" },
+      { name:"Bein Sport4", logo:"https://www.fileinwtv.site/postor/20240418192706Bein4.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-bein4_480/chunks.m3u8" },
+      { name:"Bein Sport5", logo:"https://www.fileinwtv.site/postor/20240418192717Bein5.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-bein5_480/chunks.m3u8" },
+      { name:"Bein Sport6", logo:"https://www.fileinwtv.site/postor/20240418192728Bein6.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-bein6_480/chunks.m3u8" },
+      { name:"Bein Sport7", logo:"https://www.fileinwtv.site/postor/20240418195754Bein7.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-bein7_480/chunks.m3u8" },
+      { name:"Bein Sport8", logo:"https://www.fileinwtv.site/postor/20240831143535Bein8.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-bein8_480/chunks.m3u8" },
+      { name:"Bein Sport9", logo:"https://www.fileinwtv.site/postor/20240831143312Bein9.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-bein9_480/chunks.m3u8" },
+      { name:"Bein Sport10", logo:"https://www.fileinwtv.site/postor/20240831143640Bein10.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-bein10_480/chunks.m3u8"},
+
+      { name:"True Sport1", logo:"https://www.fileinwtv.site/postor/20240418192933TrueSport1.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-tsporthd1_480/chunks.m3u8"},
+      { name:"True Sport2", logo:"https://www.fileinwtv.site/postor/20240418192951TrueSport2.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-tsporthd2_480/chunks.m3u8"},
+      { name:"True Sport3", logo:"https://www.fileinwtv.site/postor/20240418193002TrueSport3.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/th-tsporthd3_480/chunks.m3u8"},
+      { name:"True Sport4", logo:"https://www.fileinwtv.site/postor/20240418193017TrueSport4.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/hd-tsport4_480/chunks.m3u8"},
+      { name:"True Sport5", logo:"https://www.fileinwtv.site/postor/20240418193030TrueSport5.jpg", url:"https://love.r95lvvlb9o.workers.dev/vx-origin/sd-tsport5_480/chunks.m3u8"},
+      { name:"True Sport7", logo:"https://www.fileinwtv.site/postor/20240418193050TrueSport7.jpg", url:"https://love.r95lvvlb9o.workers.dev/vx-origin/sd-tsport7_480/chunks.m3u8"},
+
+      { name:"SPOTV", logo:"https://www.fileinwtv.site/postor/20240418193126SPOTV.jpg", url:"https://love.r95lvvlb9o.workers.dev/vx-origin/hd-spotv_480/chunks.m3u8"},
+      { name:"SPOTV2", logo:"https://www.fileinwtv.site/postor/20240418193135SPOTV2.jpg", url:"https://love.r95lvvlb9o.workers.dev/vx-origin/hd-spotv2_480/chunks.m3u8"},
+      { name:"T-Sports7", logo:"https://www.fileinwtv.site/postor/20240418193731tsports7.jpg", url:"https://love.r95lvvlb9o.workers.dev/lx-origin/sd-tsports_480/chunks.m3u8"},
+
+      { name:"TPF3", logo:"https://www.fileinwtv.site/postor/1763868082_0011fc8de4e840e06ced.jpg", url:"https://love.r95lvvlb9o.workers.dev/vx-origin/epl-3_480/chunks.m3u8"},
+      { name:"TPF4", logo:"https://www.fileinwtv.site/postor/20250830205132Premier_Football4.jpg", url:"https://www.livedoomovies.com:4431/02_PremierHD4_720p/playlist.m3u8" 
+
 }
 ],
 
 kids:[
-{
-name:"Cartoon Network",
-logo:"https://upload.wikimedia.org/wikipedia/commons/6/64/Cartoon_Network_2010_logo.svg",
-url:"https://example.com/cartoon.m3u8"
+      { name:"Boomerang", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-boomerang_480/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418202911Boomerang.jpg" },
+      { name:"Cartoon Network", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-cartoonnetwork_480/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418202927CN.jpg" },
+      { name:"Nickelodeon", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-nickelodeon_480/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418205237Nickelodeon.jpg" },
+      { name:"Nick jr", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-nickjr_480/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418203048Nickjr.jpg"
+
+
 }
 ],
 
 documentary:[
-{
-name:"Discovery",
-logo:"https://upload.wikimedia.org/wikipedia/commons/7/7f/Discovery_Channel_logo.svg",
-url:"https://example.com/discovery.m3u8"
+      { name:"Discovery", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-discovery_480/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418210226Discovery.jpg" },
+      { name:"Discovery Asia", url:"https://night.redfight.info/iptv.php?id=1606", logo:"https://www.fileinwtv.site/postor/20240418210201DiscoveryAsia.jpg" },
+      { name:"History", url:"https://love.pifipa2974.workers.dev/vx-origin/hd-history_720/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240423163921history.jpg" },
+      { name:"TLC", url:"https://love.pifipa2974.workers.dev/lx-origin/sd-tlc_480/chunks.m3u8", logo:"https://www.fileinwtv.site/postor/20240418210246TLC.jpg"
+
 }
 ]
 
