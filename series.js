080d3fa36d8cef2bfc1d0a1d0890437325264a17
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-04-04 20:06:11 +0700

    Update and rename series.json to series.js

---
+++ series.js
@@ -0,0 +1,9 @@
+var seriesData = {
+  series: [
+    {
+      name: "Test Series",
+      image: "https://via.placeholder.com/150",
+      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    }
+  ]
+};
