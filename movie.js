13767471b1cfed08e688ea22c82f12eaed78a1d8
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-04-04 22:39:05 +0700

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,9 +1,9 @@
 var moviesData = {
   movies: [
     {
-      name: "Big Buck Bunny",
-      image: "https://peach.blender.org/wp-content/uploads/title_anouncement.jpg",
-      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+      name: "Lefter The Story of the Ordinarius (2025) ตำนานฟุตบอลเหนือธรรมดา",
+      image: "https://www.24hd.net/wp-content/uploads/2026/04/Lefter-The-Story-of-the-Ordinarius-2025.webp",
+      url: "https://media.vdohls.com/UsH-tWS15_wIK/video.m3u8"
     }
   ]
 };
