297f1114e18451fcd73c141f0d2a5b49242026a9
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-04-04 20:21:31 +0700

    Create vip.js

---
+++ vip.js
@@ -0,0 +1,14 @@
+var vipData = {
+  vip: [
+    {
+      name: "VIP Movie 1",
+      image: "https://via.placeholder.com/150",
+      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    },
+    {
+      name: "VIP Movie 2",
+      image: "https://via.placeholder.com/150",
+      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    }
+  ]
+};
