73f1b157c7dfade348c774812efaac6ccc86e51e
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-04-04 19:14:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,9 @@
-import express from "express";
-import fetch from "node-fetch";
-import cors from "cors";
-
-const app = express();
-
-app.use(cors());
-
-app.get("/", (req,res)=>{
-res.send("TIEA IPTV PROXY RUNNING");
-});
-
-app.get("/proxy", async (req,res)=>{
-
-const url=req.query.url;
-
-if(!url){
-return res.status(400).send("No url");
+{
+  "series": [
+    {
+      "name": "Sample Series",
+      "image": "https://via.placeholder.com/300x450",
+      "url": "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    }
+  ]
 }
-
-try{
-
-const response=await fetch(url,{
-headers:{
-"Referer":"https://24playerhd.com/",
-"User-Agent":"Mozilla/5.0"
-}
-});
-
-res.set("Access-Control-Allow-Origin","*");
-
-response.body.pipe(res);
-
-}catch(err){
-
-res.status(500).send(err.toString());
-
-}
-
-});
-
-const PORT=process.env.PORT || 3000;
-
-app.listen(PORT,()=>{
-console.log("Proxy running on port "+PORT);
-});
