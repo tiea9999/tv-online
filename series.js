dc3e7aece2f2171d02759931b11ad02af534e26d
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-04-05 10:52:10 +0700

    Update series.js

--- series.js
+++ series.js
@@ -1,30 +1,10 @@
-var seriesData = {
-  series: [
-    {
-      name: "One Piece",
-      image: "https://www.24hd.net/wp-content/uploads/2026/04/One-Piece.webp",
-      episodes: [
-        { ep: "SS1 ตอนที่ 1", url: "https://media.vdohls.com/onepiece_s1_ep1.m3u8" },
-        { ep: "SS1 ตอนที่ 2", url: "https://media.vdohls.com/onepiece_s1_ep2.m3u8" },
-        { ep: "SS1 ตอนที่ 3", url: "https://media.vdohls.com/onepiece_s1_ep3.m3u8" },
-        { ep: "SS1 ตอนที่ 4", url: "https://media.vdohls.com/onepiece_s1_ep4.m3u8" }
-      ]
-    },
-    {
-      name: "Agent from Above (2026)",
-      image: "https://www.24hd.net/wp-content/uploads/2026/04/Agent-from-Above-2026.webp",
-      episodes: [
-        { ep: "ตอนที่ 1", url: "https://media.vdohls.com/6dUIi-x9Cf_ng/video.m3u8" },
-        { ep: "ตอนที่ 2", url: "https://media.vdohls.com/6dUIi-x9Cf_ng/video2.m3u8" },
-        { ep: "ตอนที่ 3", url: "https://media.vdohls.com/6dUIi-x9Cf_ng/video3.m3u8" }
-      ]
-    },
-    {
-      name: "Lefter The Story of the Ordinarius (2025)",
-      image: "https://www.24hd.net/wp-content/uploads/2026/04/Lefter-The-Story-of-the-Ordinarius-2025.webp",
-      episodes: [
-        { ep: "ตอนที่ 1", url: "https://media.vdohls.com/UsH-tWS15_wIK/video.m3u8" }
-      ]
-    }
+{
+  "name": "One Piece",
+  "image": "https://www.24hd.net/wp-content/uploads/2026/04/One-Piece.webp",
+  "episodes": [
+    {"ep": "SS1 ตอนที่ 1", "url": "https://media.vdohls.com/onepiece_s1_ep1.m3u8"},
+    {"ep": "SS1 ตอนที่ 2", "url": "https://media.vdohls.com/onepiece_s1_ep2.m3u8"},
+    {"ep": "SS1 ตอนที่ 3", "url": "https://media.vdohls.com/onepiece_s1_ep3.m3u8"},
+    {"ep": "SS1 ตอนที่ 4", "url": "https://media.vdohls.com/onepiece_s1_ep4.m3u8"}
   ]
-};
+}
