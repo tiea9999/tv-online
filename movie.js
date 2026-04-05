0f41ebcaf1544aa9725f6b7edbec9790a45f4b61
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-04-05 10:43:33 +0700

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,9 +1,9 @@
 var moviesData = {
   movies: [
-    {
-      name: "Lefter The Story of the Ordinarius (2025) ตำนานฟุตบอลเหนือธรรมดา",
-      image: "https://www.24hd.net/wp-content/uploads/2026/04/Lefter-The-Story-of-the-Ordinarius-2025.webp",
-      url: "https://media.vdohls.com/UsH-tWS15_wIK/video.m3u8"
-    }
+{
+  "name": "Lefter The Story of the Ordinarius",
+  "image": "https://www.24hd.net/wp-content/uploads/2026/04/Lefter-The-Story-of-the-Ordinarius-2025.webp",
+  "url": "https://media.vdohls.com/UsH-tWS15_wIK/video.m3u8"
+}
   ]
 };
