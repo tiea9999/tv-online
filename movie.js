161fc2f2c2f2c5231e0425de234534725b19bfe5
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-04-05 10:51:14 +0700

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,9 +1,5 @@
-var moviesData = {
-  movies: [
 {
-  "name": "Lefter The Story of the Ordinarius",
+  "name": "Lefter The Story of the Ordinarius (2025)",
   "image": "https://www.24hd.net/wp-content/uploads/2026/04/Lefter-The-Story-of-the-Ordinarius-2025.webp",
   "url": "https://media.vdohls.com/UsH-tWS15_wIK/video.m3u8"
 }
-  ]
-};
