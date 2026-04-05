f00b6aadf0efc6465f2c45bbc63c9fc52362232f
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-04-05 10:32:27 +0700

    Update series.js

--- series.js
+++ series.js
@@ -1,9 +1,12 @@
 var seriesData = {
   series: [
     {
-      "name": "Agent from Above (2026) สายลับฟ้าส่ง EP.1-8",
-      "image": "https://www.24hd.net/wp-content/uploads/2026/04/Agent-from-Above-2026.webp",
-      "url": "https://media.vdohls.com/6dUIi-x9Cf_ng/video.m3u8"
+      "name": "One Piece",
+      "image": "...",
+      "episodes": [
+        { "ep": "ตอนที่ 1", "url": "..." },
+        { "ep": "ตอนที่ 2", "url": "..." }
+      ]
     }
   ]
 };
