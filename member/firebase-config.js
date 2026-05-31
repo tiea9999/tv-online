214763013966941f9728144b0a001d8cfebce094
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-05-31 11:31:13 +0700

    Create firebase-config.js

---
+++ member/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyD56tjCIPBM3nctC5P2NzBgLOShcbx1w8A",
+  authDomain: "tiea-iptv.firebaseapp.com",
+  projectId: "tiea-iptv",
+  storageBucket: "tiea-iptv.firebasestorage.app",
+  messagingSenderId: "1031384335454",
+  appId: "1:1031384335454:web:34e41ad2d93705834920cf",
+  measurementId: "G-Z9G1KQJ3HB"
+};
+export { firebaseConfig };
