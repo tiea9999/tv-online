31a8361c9150ace197728d27c96239cdd91f193f
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2026-06-08 15:24:38 +0700

    Delete movie.js

--- movie.js
+++
@@ -1,5 +0,0 @@
-{
-  "name": "Lefter The Story of the Ordinarius (2025)",
-  "image": "https://www.24hd.net/wp-content/uploads/2026/04/Lefter-The-Story-of-the-Ordinarius-2025.webp",
-  "url": "https://media.vdohls.com/UsH-tWS15_wIK/video.m3u8"
-}
